2a5088a5d5e264dc8126368ea20af4dd7c0cb99b
CHANDAN SINGH <90786270+TechyCSR@users.noreply.github.com>
2024-06-30 12:35:31 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,3 +1,12 @@
+import os
 from flask import Flask
 
-app = Flask(__name__, template_folder='/client/html')
+base_dir = os.path.dirname(os.path.abspath(__file__))
+template_dir = os.path.join(base_dir, '../client/html')
+
+app = Flask(__name__, template_folder=template_dir)
+
+# Define your routes and other configurations
+
+if __name__ == "__main__":
+    app.run(debug=True)
